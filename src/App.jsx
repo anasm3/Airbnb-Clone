50ccb45f191f1a53f054f2219238dcced8c1f964
anasm3 <anasmukhtar08@gmail.com>
2024-09-14 12:19:02 +0500

    Final Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import Navbar from "./components/Navbar";
-import Hero from "./components/hero";
+import Hero from "./components/Hero";
+import Card from "./components/card";
+import data from "./data";
 
 export default function App() {
+    const cards = data.map(item => {
+        return (
+            <Card
+                key={item.id}
+                item={item}
+            />
+        )
+    })        
+    
     return (
         <div>
             <Navbar />
             <Hero />
+            <section className="cards-list">
+                {cards}
+            </section>
         </div>
     )
 }
